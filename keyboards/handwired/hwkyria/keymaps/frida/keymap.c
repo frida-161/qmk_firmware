e28361b1afb38fe045b097f927b91a61caea0ffd
frida <7078986+frida-161@users.noreply.github.com>
2026-04-09 11:24:53 +0200

    symbol layer

--- keyboards/handwired/hwkyria/keymaps/frida/keymap.c
+++ keyboards/handwired/hwkyria/keymaps/frida/keymap.c
@@ -2,10 +2,12 @@
 
 smtd_resolution on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
     switch (keycode) {
-        SMTD_MT(KC_N, KC_LEFT_GUI)
         SMTD_MT(KC_R, KC_LEFT_ALT)
         SMTD_MT(KC_T, KC_LEFT_CTRL)
         SMTD_MT(KC_S, KC_LSFT)
+        SMTD_MT(KC_E, KC_LEFT_ALT)
+        SMTD_MT(KC_A, KC_LEFT_CTRL)
+        SMTD_MT(KC_H, KC_LSFT)
     }
 
     return SMTD_RESOLUTION_UNHANDLED;
